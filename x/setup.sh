54677af21ad42e36e8340232ce26be3d8c405a00
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-04-05 21:57:54 -0500

    X: Add auto monitors/sound setup (Home, No-Home)

---
+++ x/setup.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+# Auto setup configuration script
+# [!] Execute this AFTER ratpoison
+
+# 2 different setups are available:
+#
+# * Home
+#       - Laptop display [1366x768]
+#       - LCD (VGA) monitor [1024x768]
+#       - Laptop above LCD monitor
+#       - Web explorer
+#       - Sound available
+#
+# * No-Home
+#       - Laptop display [1366x768]
+#       - Sound mute
+
+
+
+# Return TRUE if VGA is connected
+#
+# VGA plugedin: Home
+# VGA NO plugedin: No-Home
+vga_plugedin()
+{
+    VGASTATE=$(xrandr | grep -i 'vga' | awk '{print $2 }')
+
+    if [[ $VGASTATE == "connected" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+# Home setup
+if vga_plugedin; then
+    xrandr --output VGA1 --mode 1024x768
+    xrandr --output LVDS1 --mode 1366x768 --above VGA1
+    amixer set Master unmute
+    firefox&
+
+# No-Home setup
+else
+    xrandr --output LVDS1 --mode 1366x768
+    amixer set Master mute
+fi
--- x/wifi.sh
+++ x/wifi.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
-## Auto connecto to Wi-Fi network defined at /etc/wpa_supplicant/wifi.h.conf
-sudo wpa_supplicant -i wlp2s0 -c /etc/wpa_supplicant/wifi.h.conf -B
-sudo dhclient -nw wlp2s0
+
+# Auto wifi network connection script
+#
+# Invoke from Shell very first initialization
+
+# If no parameters are given connect to home network
+if [[ "$#" -eq 0 ]]; then
+    sudo wpa_supplicant -i wlp2s0 -c /etc/wpa_supplicant/wifi.h.conf -B
+    sudo dhclient -nw wlp2s0
+fi
