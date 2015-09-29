52d807ac1a97a83d8ddae2303b703919973fa4b9
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-09-29 17:55:19 -0500

    Scripts: Add restore_ratpoison

---
+++ scripts/restore_ratpoison.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Metacity is used as alternative window manager, get rid of it properly
+# and restart ratpoison
+
+read -n1 -r -p "Ratpoison is about to be restarted, are you sure? [y/n]" key
+
+if [ "$key" = 'y' ]; then
+    killall metacity
+    ratpoison -c restart
+else
+    echo
+    echo "Ok then..."
+    echo
+    exit 0
+fi
