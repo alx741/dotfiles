36a49c89f4a9523d3aa5030e97c00a8f8bf72c98
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-10-01 11:30:34 -0500

    Add script for switching or starting software

---
+++ scripts/select.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+function is_running
+{
+    PID=$(pidof -s $1)
+
+    if [[ $PID != "" ]]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+function launch_firefox
+{
+    if is_running 'firefox';
+    then
+        ratpoison -c "select firefox"
+    else
+        firefox&
+    fi
+}
+
+
+function launch_terminal
+{
+    if is_running 'urxvt';
+    then
+        ratpoison -c "select urxvt"
+    else
+    urxvt -e bash -c "tmux -q has-session && exec tmux attach-session -d || exec tmux new-session -n$USER -s$USER@$HOSTNAME"&
+    fi
+}
+
+
+case "$1" in
+    'firefox')
+        launch_firefox
+        ;;
+    'terminal')
+        launch_terminal
+        ;;
+esac
