3dd8d9df46d8b7bdef2024db37b69e17f7c4a110
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-10-05 18:09:24 -0500

    Ratpoison: Improve backlight control

--- scripts/.scripts/backlight_control.sh
+++ scripts/.scripts/backlight_control.sh
@@ -1,13 +1,45 @@
 #! /bin/sh
 
 
+backlight_value=""
+
+function get_backlight_value
+{
+    backlight_value=`xbacklight | sed -e 's/\..*//'`
+}
+
+
+function toggle
+{
+    # Toggle between 100% and 60%
+    get_backlight_value
+
+    if [[ $backlight_value -ne 60 && $backlight_value -ne 100 ]]
+    then
+        xbacklight -set 60
+    fi
+
+    if [[ $backlight_value -eq 100 ]]
+    then
+        xbacklight -set 60
+    else
+        xbacklight -set 100
+    fi
+}
+
+
+
 case "$1" in
-    '+')
+    'inc')
         xbacklight -inc 20
         ;;
-    '-')
+    'dec')
         xbacklight -dec 20
+        ;;
+    'toggle')
+        toggle
         ;;
 esac
 
-ratpoison -c "echo $(xbacklight | sed -e 's/\..*//')"
+get_backlight_value
+ratpoison -c "echo {$backlight_value}"
