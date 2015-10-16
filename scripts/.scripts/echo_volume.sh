4f2947aa3f274acad1fc8601abad607e7523f391
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-10-16 15:09:17 -0500

    Scripts: Fix volume information echoing

--- scripts/.scripts/echo_volume.sh
+++ scripts/.scripts/echo_volume.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-sound_data=$(amixer sget Master | tail -n 1 | awk '{print $5" "$6}')
-sound_volume=$(echo $sound_data | awk '{print $1}' | tr -d '[]%')
-sound_mute=$(echo $sound_data | awk '{print $2}' | tr -d '[]')
+sound_data=$(amixer sget Master | tail -n 1)
+sound_volume=$(echo $sound_data | awk '{print $4}' | tr -d '[]%')
+sound_mute=$(echo $sound_data | awk '{print $6}' | tr -d '[]')
 
 sound_state="$(echo $sound_mute | sed 's/on/-/;s/off/M/') \
 $(echo $sound_volume)"
