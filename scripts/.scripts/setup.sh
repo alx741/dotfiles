1d34ceaa835e4763f685957ec181db9cb84b1fa4
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-10-27 15:06:01 -0500

    X: setup: Change display power saving settings

--- scripts/.scripts/setup.sh
+++ scripts/.scripts/setup.sh
@@ -10,6 +10,7 @@
 #       - LCD (HDMI) monitor [1360x768]
 #       - LCD (VGA) monitor [1024x768]
 #       - HDMI left of VGA
+#       - No display power saving
 #       - Sound unmute
 #       - Ethernet static configuration
 #       - Init:
@@ -19,6 +20,7 @@
 #       - Laptop display [1366x768]
 #       - LCD (VGA) monitor [1024x768] (if any)
 #       - Laptop below LCD monitor (if any)
+#       - Display power saving
 #       - Sound mute
 #       - Ethernet dhcp client
 
@@ -39,7 +41,6 @@ function am_i_at_home
     fi
 }
 
-
 function is_vga_plugedin
 {
     VGASTATE=$(xrandr | grep -i 'vga' | awk '{print $2 }')
@@ -58,6 +59,8 @@ then
     xrandr --output LVDS1 --off
     xrandr --output HDMI1 --mode 1360x768
     xrandr --output VGA1 --mode 1024x768 --left-of HDMI1
+    xbacklight -set 0
+    xset dpms 0 0 0
     amixer set Master unmute
     amixer set Master 50%
     ~/.scripts/network/configure_ether.sh
@@ -69,6 +72,7 @@ else
         xrandr --output VGA1 --mode 1366x768 --above LVDS1
     fi
     xbacklight -set 100
+    xset dpms 100 200 250
     amixer set Master 0%
     amixer set Master mute
     dhclient enp3s0
