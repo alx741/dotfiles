b2917ddbb2164bcc83ceae75253eca6804033f55
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-11-03 16:31:09 -0500

    Ratpoison: taskwarrior control: "add" feature

--- scripts/.scripts/ratpoison/task_control.sh
+++ scripts/.scripts/ratpoison/task_control.sh
@@ -1,11 +1,30 @@
 #! /bin/sh
 
 
+function add_task()
+{
+    task=`ratpoison -c "prompt Add task: "`
+    if [[ "$task" == " " ]] || [[ "$task" == "" ]]; then exit 0; fi
+
+    task add $task
+
+    if [[ $? -ne 0 ]]
+    then
+        ratpoison -c "echo [!] Bad syntax"
+        exit 1
+    fi
+}
+
+
+
 case "$1" in
     'list')
         ratpoison -c "echo `task list`"
         ;;
     'list_projects')
         ratpoison -c "echo `task projects`"
+        ;;
+    'add')
+        add_task
         ;;
 esac
