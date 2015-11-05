ff61860fa99521f2476ae0a940155c77d9b16a83
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-11-05 17:57:30 -0500

    Ratpoison: restore_ratpoison: remove confirmation

--- scripts/.scripts/ratpoison/restore_ratpoison.sh
+++ scripts/.scripts/ratpoison/restore_ratpoison.sh
@@ -3,15 +3,6 @@
 # Openbox is used as alternative window manager, get rid of it properly
 # and restart ratpoison
 
-read -n1 -r -p "Ratpoison is about to be restarted, are you sure? [y/n]" key
-
-if [ "$key" = 'y' ]; then
-    killall openbox
-    sleep 1
-    ratpoison -c restart
-else
-    echo
-    echo "Ok then..."
-    echo
-    exit 0
-fi
+killall openbox
+sleep 1
+ratpoison -c restart
