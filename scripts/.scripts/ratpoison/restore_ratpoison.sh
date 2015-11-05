dcb1c3e1c8774654a9238f36258bd89f19839804
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-11-05 00:13:15 -0500

    Ratpoison: Use openbox as the temporal window manager

--- scripts/.scripts/ratpoison/restore_ratpoison.sh
+++ scripts/.scripts/ratpoison/restore_ratpoison.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Metacity is used as alternative window manager, get rid of it properly
+# Openbox is used as alternative window manager, get rid of it properly
 # and restart ratpoison
 
 read -n1 -r -p "Ratpoison is about to be restarted, are you sure? [y/n]" key
 
 if [ "$key" = 'y' ]; then
-    killall metacity
+    killall openbox
     sleep 1
     ratpoison -c restart
 else
