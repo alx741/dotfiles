b50b1d61c5965d9f9b80d3e1dab55c75efc9a50e
Daniel Campoverde [alx741] <alx741@riseup.net>
2015-11-30 08:45:21 -0500

    Scripts: ratpoison: execute xcowsay in background

--- scripts/.scripts/ratpoison/et_phone_home.sh
+++ scripts/.scripts/ratpoison/et_phone_home.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-xcowsay --image ~/.scripts/ratpoison/et.png "ET phone home..." --think
+xcowsay --image ~/.scripts/ratpoison/et.png "ET phone home..." --think &
 ~/.scripts/ratpoison/app_select.sh terminal
 tmux new-window -c ~ -n "hades" ~/.scripts/network/et_phone_home.sh
