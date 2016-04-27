fba4a6df3007d154916ad5ee45d111eceffe76d8
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-04-27 15:12:17 -0500

    Ratpoison: Use FZF for fuzzy window selection

--- scripts/.scripts/ratpoison/window_select.sh
+++ scripts/.scripts/ratpoison/window_select.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 
 
-window_list=`ratpoison -c "windows"`
+function ratmen_select
+{
+    window_list=$(ratpoison -c "windows")
 
-vmenu_cmd="vmenu --title Windows "
-for line in $window_list
-do
-    number=`echo $line | head -c 1`
-    vmenu_cmd+="'$line' "
-    vmenu_cmd+="'ratpoison -c \"select $number\"' "
-done
+    vmenu_cmd="vmenu --title Windows "
+    for line in $window_list
+    do
+        number=$(echo $line | head -c 1)
+        vmenu_cmd+="'$line' "
+        vmenu_cmd+="'ratpoison -c \"select $number\"' "
+    done
 
-eval $vmenu_cmd
+    eval $vmenu_cmd
+}
+
+
+function fzf_select
+{
+    pattern=$(ratpoison -c "prompt > ")
+    if [[ "$pattern" == "" ]];
+    then
+        exit 0
+    fi
+
+    window_list=$(ratpoison -c "windows %c")
+    selected=$(echo "$window_list" | fzf -q "$pattern" -1 -0)
+
+    if [[ "$selected" != "" ]];
+    then
+        ratpoison -c "select $selected"
+    else
+        ratpoison -c "echo [!] There is no a matching window for \"$pattern\""
+    fi
+}
+
+
+case $1 in
+    'ratmen')
+        ratmen_select
+        ;;
+    'fzf')
+        fzf_select
+        ;;
+esac
