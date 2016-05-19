8dddf9e7739f4b47f5fa09ff3838cc1ff897acff
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-05-19 17:45:29 -0500

    Scripts: Better git pushing

--- scripts/.scripts/fancy/git.sh
+++ scripts/.scripts/fancy/git.sh
@@ -5,6 +5,7 @@ echo
 cat ~/.ascii_art/octocat
 echo
 
+
 function clone_clipboard
 {
     url=`xclip -selection clipboard -o`
@@ -21,9 +22,36 @@ function clone_clipboard
 }
 
 
+function push
+{
+    current_branch=`git branch --list --no-color | grep "*" \
+        | sed -e 's/[\*[:space:]]//g'`
+
+    if [[ "$current_branch" == "master" ]];
+    then
+        echo
+        echo "Pushing to master"
+        echo
+        git push origin master
+    else
+        echo
+        read -p "Push to $current_branch? [y/N] " push
+        echo
+
+        if [[ "$push" == "y" ]];
+        then
+            echo
+            echo "Pushing to $current_branch"
+            echo
+            git push origin "$current_branch"
+        fi
+    fi
+}
+
+
 case $1 in
     'push')
-        git push origin master
+        push
         ;;
     'pull')
         git pull origin master
