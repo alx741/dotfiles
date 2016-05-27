41752a6954ad29907ce75accd8c4978b52e21201
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-05-27 00:50:24 -0500

    Mozrepl: Search tabs starting from the end

--- mozrepl/.mozrepl/select_tab.js
+++ mozrepl/.mozrepl/select_tab.js
@@ -2,7 +2,7 @@ function selectTab(page)
 {
     var numTabs=gBrowser.browsers.length;
     var url="";
-    for(i=0; i<numTabs-1; i++)
+    for(i=numTabs-1; i>0; i--)
     {
         url= gBrowser.browsers[i].contentDocument.location.href.split(".")[1];
         if(url == page)
