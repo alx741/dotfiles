be8f1e72fc480b79c6a941bb156a1aa65261493b
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-06-13 00:41:31 -0500

    Vimfx: Use gH and gL to move tabs

--- vimfx/.vimfx/config.js
+++ vimfx/.vimfx/config.js
@@ -9,8 +9,13 @@ vimfx.set('mode.normal.scroll_page_up', 'b <c-b>')
 
 vimfx.set('mode.normal.tab_select_next', 'gt <c-k> <c-n> l')
 vimfx.set('mode.normal.tab_select_previous', 'gT <c-j> <c-p> h')
+vimfx.set('mode.normal.tab_move_forward', 'gL')
+vimfx.set('mode.normal.tab_move_backward', 'gH')
 vimfx.set('mode.normal.window_new', '')
 
 vimfx.set('prevent_autofocus', true)
 
 vimfx.set('mode.normal.scroll_to_mark', '` \'')
+
+vimfx.set('mode.normal.history_list', '')
+vimfx.set('mode.normal.tab_select_oldest_unvisited', '')
