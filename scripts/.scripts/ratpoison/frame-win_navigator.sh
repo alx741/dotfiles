3e0c43d8381baddd49d7b4f98428f6f1f71a468b
Daniel Campoverde [alx741] <alx741@riseup.net>
2016-06-23 18:18:19 -0500

    Ratpoison: Add frame-window navigator

---
+++ scripts/.scripts/ratpoison/frame-win_navigator.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+ratpoison -c 'fdump'| sed 's/,/\n/g' | awk '{print $5" "$19}' > ratpoison_fdump
+
+# Calculate X coordinate for rightmost frame
+greater_x_coordinate=0
+while read frame;
+do
+    coordinate=$(echo "$frame" | cut -d' ' -f1)
+    if [[ $coordinate -gt $greater_x_coordinate ]];
+    then
+        greater_x_coordinate=$coordinate
+    fi
+done < ratpoison_fdump
+
+
+# Calculate current frame X coordinate
+x_coordinate=$(head -n1 ratpoison_fdump | cut -d' ' -f1)
+last_access=$(head -n1 ratpoison_fdump | cut -d' ' -f2)
+while read frame;
+do
+    access=$(echo "$frame" | cut -d' ' -f2)
+    if [[ $access -gt $last_access ]];
+    then
+        last_access=$access
+        x_coordinate=$(echo "$frame" | cut -d' ' -f1)
+    fi
+done < ratpoison_fdump
+
+function is_leftmost
+{
+    if [[ $x_coordinate -eq 0 ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function is_rightmost
+{
+    if [[ $x_coordinate -eq $greater_x_coordinate ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+# Go to previous screen if currently if leftmost frame
+# Go to next screen if currently if rightmost frame
+# Execute frame focus otherwise
+if [[ "$1" == "left" ]];
+then
+    if is_leftmost; then
+        ratpoison -c 'prevscreen'
+    else
+        ratpoison -c 'focusleft'
+    fi
+elif [[ "$1" == "right" ]];
+then
+    if is_rightmost; then
+        ratpoison -c 'nextscreen'
+    else
+        ratpoison -c 'focusright'
+    fi
+fi
