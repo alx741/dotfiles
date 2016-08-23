5609f2b4413f2ef475c157c960215248de136170
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-08-22 19:21:40 -0500

    Add fancy colorized Haskell GHCI

---
+++ scripts/.scripts/fancy/ghci.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+GREEN=`echo -e '\e[32m'`
+RED=`echo -e '\e[31m'`
+BLUE=`echo -e '\e[34m'`
+CYAN=`echo -e '\e[36m'`
+MAGENTA=`echo -e '\e[35m'`
+RESET=`echo -e '\e[0m'`
+
+stack ghci "$@" 2>&1 |\
+    sed "
+        s/\[/${MAGENTA}&${RESET}/g;\
+        s/\]/${MAGENTA}&${RESET}/g;\
+        s/\]/${MAGENTA}&${RESET}/g;\
+        s/[()]/${BLUE}&${RESET}/g;\
+        s/->/${CYAN}&${RESET}/g;\
+        s/=>/${MAGENTA}&${RESET}/g;\
+        s/^Failed, modules loaded:/${RED}&${RESET}/g;\
+        s/^Ok, modules loaded:/${GREEN}&${RESET}/g;\
+        s/::/${MAGENTA}&${RESET}/g;\
+        "
