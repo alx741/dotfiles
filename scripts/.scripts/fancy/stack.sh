b7f530572e50218ee526cd15ac8e90a15744deae
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-12-05 14:06:50 -0500

    Less fancier Haskell Stack fancy script

--- scripts/.scripts/fancy/stack.sh
+++ scripts/.scripts/fancy/stack.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
-GREEN=`echo -e '\e[32m'`
-RED=`echo -e '\e[31m'`
-BLUE=`echo -e '\e[34m'`
-CYAN=`echo -e '\e[36m'`
-MAGENTA=`echo -e '\e[35m'`
-RESET=`echo -e '\e[0m'`
-
 echo
 cat ~/.ascii_art/lambda | lolcat -S 28
 echo
 
-params=$(echo "$@" | cut -d' ' -f2-)
+if [[ $# -gt 1 ]]; then
+    params=$(echo "$@" | cut -d' ' -f2-)
+fi
 
 case $1 in
     'ghci')
-        stack ghci "$params"
+        stack ghci
         ;;
     'ghc')
         stack ghc "$params"
