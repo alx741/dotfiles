6fc7f344397b8604c18d654106a8ca55c562553b
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-12-13 19:38:11 -0500

    Scripts: WiFi: List networks

--- scripts/.scripts/network/configure_wifi.sh
+++ scripts/.scripts/network/configure_wifi.sh
@@ -2,27 +2,78 @@
 
 source "$(dirname "$0")/../utility.sh"
 
-
-if am_i_at_home || [[ "$1" == "home" ]];
-then
-    sudo ifconfig wlp2s0 up
-    sudo wpa_supplicant -i wlp2s0 -c /etc/wpa_supplicant/wifi.h.conf -B
-    sudo dhclient -nw wlp2s0
-else
-    sudo ifconfig wlp2s0 up
-    sudo iwconfig wlp2s0 essid UCWIFI ap any
-    sudo dhclient -nw wlp2s0
-fi
+IF=wlp2s0
 
 
-# Create adhoc network sharing through cable interface
-if [[ "$1" == "adhoc" ]]; then
-    sudo ifconfig wlp2s0 down
-    sudo iwconfig wlp2s0 mode ad-hoc
-    sudo iwconfig wlp2s0 essid "ushare" key "ushare10"
-    sudo ifconfig wlp2s0 192.168.5.1
+function exists_home_wifi
+{
+    wifi_scan=$(sudo iwlist "$IF" scan | grep -i "ALX")
+    if [[ $wifi_scan == "" ]]; then return 1; else return 0; fi
+}
+
+function exists_u_wifi
+{
+    wifi_scan=$(sudo iwlist "$IF" scan | grep -i "UCWIFI")
+    if [[ $wifi_scan == "" ]]; then return 1; else return 0; fi
+}
+
+function creat_adhoc
+{
+    sudo ifconfig "$IF" down
+    sudo iwconfig "$IF" mode ad-hoc
+    sudo iwconfig "$IF" essid "ushare" key "ushare10"
+    sudo ifconfig "$IF" 192.168.5.1
     sudo iptables -t nat -A POSTROUTING -o enp3s0 -j MASQUERADE
     sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
-    sudo dhclient wlp2s0
-fi
+}
 
+function wifi_list
+{
+    data=$(sudo iwlist scanning 2> /dev/null)
+
+    echo
+    while read line; do
+        [[ "$line" =~ \(Channel ]] && {
+            chn=${line##*nel }
+            chn=${chn:0:$((${#chn}-1))}
+        }
+        [[ "$line" =~ Quality ]] && {
+            qual=${line##*ity=}
+            qual=${qual%% *}
+        }
+        # [[ "$line" =~ WEP ]] && enc="WEP"
+        # [[ "$line" =~ WPA ]] && enc="WPA/WPA2"
+        [[ "$line" =~ ESSID ]] && {
+            essid=${line##*ID:}
+            echo "$essid | channel: $chn | quality: $qual $enc"
+        }
+    done <<< "$data"
+}
+
+
+
+case $1 in
+    'list')
+        wifi_list
+        exit
+        ;;
+    'adhoc')
+        creat_adhoc
+        ;;
+    *)
+        sudo ifconfig "$IF" down 2> /dev/null
+        sudo killall wpa_supplicant 2> /dev/null
+        sudo killall dhclient 2> /dev/null
+        sudo ifconfig "$IF" up 2> /dev/null
+
+        if exists_home_wifi; then
+            sudo wpa_supplicant -i "$IF" -c /etc/wpa_supplicant/alx.conf -B
+        elif exists_home_wifi; then
+            sudo iwconfig "$IF" essid UCWIFI ap any
+        fi
+        ;;
+esac
+
+
+sudo iwconfig "$IF" commit 2> /dev/null
+sudo dhclient -nw "$IF"
