025dd9015fd6918e1e1a5a618ee884fbdce5e3ce
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-12-20 20:41:37 -0500

    Scripts: Handle pacman and packer tricks

---
+++ scripts/.scripts/pacman.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+CACHE_FILE="/tmp/pacman-packer_last_searched_package"
+
+echo
+cat ~/.ascii_art/pacman
+echo
+
+if [[ "$2" == "-Ss" ]]; then
+    echo "$3" > "$CACHE_FILE"
+fi
+
+
+case $1 in
+    'pacman')
+        shift 1
+        sudo pacman "$@"
+        ;;
+    'packer')
+        shift 1
+        packer --noedit --noconfirm "$@"
+        ;;
+    'getit')
+        packer --noedit --noconfirm -S $(cat "$CACHE_FILE")
+        ;;
+    'update')
+        sudo pacman -Syyu
+        ;;
+esac
