90a8e7dd829937f4d48a130ae0eb4c57255bc2fe
Daniel Campoverde [alx741] <alx@sillybytes.net>
2016-12-25 10:18:29 -0500

    Scripts: Update Arch deps

--- scripts/.scripts/install_arch_deps.sh
+++ scripts/.scripts/install_arch_deps.sh
@@ -4,9 +4,10 @@ DEPS=(
     astyle avrdude ccze ctags ghc git gnupg hindent mongodb mpd mplayer mutt
     mariadb ncmpcpp neovim nethack newsbeuter nitrogen openbox pacman ratpoison
     readline stack sxiv task tmux rxvt-unicode gvim vim-runtime w3m weechat
-    xmonad zathura zsh acpi alsa-utils ccze dhclient diff-so-fancy expect figlet
-    firefox id3v2 lolcat mpc qrencode scrot task unclutter xclip xdotool
-    youtube-dl
+    xmonad zathura zathura-djvu zathura-pdf-mupdf zsh acpi alsa-utils ccze
+    dhclient diff-so-fancy expect figlet firefox id3v2 lolcat mpc qrencode scrot
+    task unclutter xclip xdotool youtube-dl reflector stow fzf fasd wget ntp
+    sshpass
 )
 
 sudo pacman -Syy ${DEPS[@]}
