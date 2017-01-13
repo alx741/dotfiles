4ffbe9259854bfd35f35d660275262491b00562b
Daniel Campoverde [alx741] <alx@sillybytes.net>
2017-01-13 13:58:04 -0500

    Scripts: Add more dependencies

---
+++ scripts/.scripts/install_deps.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+ARCH_DEPS=(
+    astyle avrdude ccze ctags ghc git gnupg hindent mongodb mpd mplayer mutt
+    mariadb ncmpcpp neovim nethack newsbeuter nitrogen openbox pacman ratpoison
+    readline stack sxiv task tmux rxvt-unicode gvim vim-runtime w3m weechat
+    xmonad zathura zathura-djvu zathura-pdf-mupdf zsh acpi alsa-utils ccze
+    dhclient diff-so-fancy expect figlet firefox id3v2 lolcat mpc qrencode scrot
+    task unclutter xclip xdotool youtube-dl reflector stow fzf fasd wget ntp
+    sshpass grep sed curl jshon expac haskell-hscolour
+)
+
+AUR_DEPS=(
+)
+
+HASK_DEPS=(
+    shake yesod hoogle hasktags
+)
+
+sudo pacman -Syy ${ARCH_DEPS[@]}
+
+if ! type "packer" > /dev/null; then
+    install_packer
+fi
+packer --noedit --noconfirm -S ${AUR_DEPS[@]}
+
+stack install ${HASK_DEPS[@]}
+
+
+function install_packer()
+{
+    cd ~
+    mkdir packer
+    cd packer
+    wget 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer'
+    mv 'PKGBUILD?h=packer' PKGBUILD
+    makepkg
+    sudo pacman -U packer*.tar.xz
+    cd ..
+    rm -rfv packer
+}
