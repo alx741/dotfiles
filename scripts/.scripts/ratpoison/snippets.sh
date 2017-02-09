318c6526171db9ece1e8dcf9e3a9554daa54c79f
Daniel Campoverde [alx741] <alx@sillybytes.net>
2017-02-08 20:40:23 -0500

    Scripts: Port snippets script to rofi

--- scripts/.scripts/ratpoison/snippets.sh
+++ scripts/.scripts/ratpoison/snippets.sh
@@ -5,18 +5,11 @@ SNIP_DIR=~/.snippets
 
 function snippet_menu
 {
-    vmenu_cmd="vmenu --unfocus-exit --title Snippets -p "
     for snip in $SNIP_DIR/*
     do
-        vmenu_cmd+="${snip##*/}"
-        vmenu_cmd+=" '$snip' "
+        snippets+="${snip##*/}\n"
     done
-    selected_snippet=`eval $vmenu_cmd`
-
-    if [[ $selected_snippet != "" ]]
-    then
-        echo "$selected_snippet"
-    fi
+    echo -e "$snippets" | sed 's/URxvt/URxvt terminal/' | rofi -dmenu -i -p "> " -no-custom
 }
 
 
@@ -26,7 +19,7 @@ function select_snippet
 
     if [[ $snippet != "" ]]
     then
-        xclip -selection clipboard -i "$snippet"
+        xclip -selection clipboard -i "$SNIP_DIR/$snippet"
     else
         return 0
     fi
