023fd2282cdc3761ece84a3d20e105eff75ce1ec
Daniel Campoverde <alx@sillybytes.net>
2017-11-09 12:36:21 -0500

    Scripts: New system deps

--- scripts/.scripts/install_deps.sh
+++ scripts/.scripts/install_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ARCH_DEPS=(
-    astyle avrdude ccze ctags ghc cabal-install git gnupg hindent mongodb mpd
+    astyle ccze ctags ghc cabal-install git gnupg hindent mongodb mpd
     mplayer mutt mariadb ncmpcpp neovim nethack newsbeuter nitrogen openbox
     pacman ratpoison readline stack sxiv task tmux rxvt-unicode gvim vim-runtime
     w3m weechat xmonad zathura zathura-djvu zathura-pdf-mupdf zsh acpi
@@ -11,7 +11,7 @@ ARCH_DEPS=(
     ttf-dejavu ttf-roboto xcape transmission-gtk gimp inkscape asciidoctor
     xorg-xsetroot xf86-input-synaptics xbindkeys xorg-xrandr zip unrar calc
     the_silver_searcher npm htop chromium cloc pandoc dnsutils bind-tools unzip
-    wol tree
+    wol tree avr-binutils avr-gcc avr-gdb avr-libc avrdude simarv
 )
 
 AUR_DEPS=(
