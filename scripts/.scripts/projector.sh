2f523986695d2ffed46ca767f65bf68d3ebd578c
Daniel Campoverde <alx@sillybytes.net>
2018-04-03 11:10:18 -0500

    Scripts: Fix projector handler script

--- scripts/.scripts/projector.sh
+++ scripts/.scripts/projector.sh
@@ -6,11 +6,11 @@ source "$(dirname "$0")/utility.sh"
 if is_vga_plugedin
 then
     if [[ "$1" == "dual" ]]; then
-        xrandr --output LVDS1 --mode 1366x768
-        xrandr --output VGA1 --mode 1024x768 --above LVDS1
+        xrandr --output LVDS-1 --mode 1366x768
+        xrandr --output VGA-1 --mode 1024x768 --above LVDS-1
     elif [[ "$1" == "in" ]]; then
-        xrandr --output LVDS1 --mode 1024x768
-        xrandr --output VGA1 --same-as LVDS1 --mode 1024x768
+        xrandr --output LVDS-1 --mode 1024x768
+        xrandr --output VGA-1 --same-as LVDS-1 --mode 1024x768
     else
         echo_usage
     fi
@@ -20,8 +20,8 @@ else
         echo "[!] VGA not connected"
         echo
     elif [[ "$1" == "out" ]]; then
-        xrandr --output LVDS1 --mode 1366x768
-        xrandr --output VGA1 --off
+        xrandr --output LVDS-1 --mode 1366x768
+        xrandr --output VGA-1 --off
     fi
 fi
 
@@ -29,7 +29,7 @@ function echo_usage
 {
     echo
     echo  in    - mirror to VGA
-    echo  dual  - independent VGA and LVDS1
+    echo  dual  - independent VGA and LVDS-1
     echo  out   - back to normal
     echo
 }
