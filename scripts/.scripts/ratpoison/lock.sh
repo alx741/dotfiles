0054cf503b25edcdecfdab121065efbc3a6dc509
Daniel Campoverde <alx@sillybytes.net>
2019-10-08 15:57:53 -0500

    Ratpoison: screen locking

---
+++ scripts/.scripts/ratpoison/lock.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xlock -mode blank -usefirst +description +echokeys +mousemotion +sound +showdate -timeout 1 -fg "#657B83" -bg "#FDF6E3" -username " " -password " " -info " " -validate "..." -invalid "oops"
