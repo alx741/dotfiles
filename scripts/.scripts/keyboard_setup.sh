133e699864e9db4a485c3297fd38479a058fdb11
Daniel Campoverde <alx@sillybytes.net>
2019-10-14 18:35:26 -0500

    Scripts: keyboard identification

--- scripts/.scripts/keyboard_setup.sh
+++ scripts/.scripts/keyboard_setup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+builtin_id=$(xinput -list | grep -i 'AT Translated' | grep -oh 'id=[0-9]*' | cut -d'=' -f 2)
+venabili_id=$(xinput -list | grep -i 'Venabili' | tail -n 1 | grep -oh 'id=[0-9]*' | cut -d'=' -f 2)
+
+echo "$builtin_id"
+echo "$venabili_id"
+
 # Repeat rate
 xset r rate 200 50
 
@@ -10,8 +16,8 @@ xbindkeys&
 setxkbmap us -variant altgr-intl
 
 # Configure keys
-setxkbmap -option ctrl:swapcaps&
-xmodmap ~/.xmodmap&
+setxkbmap -device "$builtin_id" -option ctrl:swapcaps&
+# xmodmap ~/.xmodmap&
 killall xcape
 xcape -t 250 -e 'Control_L=Escape'
 xcape -e 'Super_L=Control_L|G|bracketleft'
