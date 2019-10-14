00b582337e21fda589f6d21dafae9a409ecdd38a
Daniel Campoverde <alx@sillybytes.net>
2019-10-14 18:38:10 -0500

    Auto run mutt

--- scripts/.scripts/ratpoison/app_select.sh
+++ scripts/.scripts/ratpoison/app_select.sh
@@ -3,7 +3,7 @@
 
 function is_running
 {
-    PID=$(pidof -s $1)
+    PID=$(pidof -s "$1")
 
     if [[ $PID != "" ]]
     then
@@ -34,7 +34,7 @@ function launch_terminal
     else
         urxvt -e bash -c "tmux -2 -q has-session &>/dev/null && exec tmux \
             attach-session -d || exec tmux new-session -n$USER \
-            -s$USER@$HOSTNAME weechat\; new-window"&
+            -s$USER@$HOSTNAME mutt\; new-window weechat\; new-window"&
     fi
 }
 
