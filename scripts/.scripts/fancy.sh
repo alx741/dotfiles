565aaba3f078ffc14a3acc04acd66d2d3f4f832f
Daniel Campoverde <alx@sillybytes.net>
2019-10-14 18:31:20 -0500

    Scripts: fancy alerts

--- scripts/.scripts/fancy.sh
+++ scripts/.scripts/fancy.sh
@@ -1,1 +1,15 @@
-/home/alx/lab/sh-fancy/fancy.sh
+function displayInfo {
+    echo -e "\e[46m\e[37m[•]\e[0m \e[36m$1\e[0m"
+}
+
+function displaySuccess {
+    echo -e "\e[42m\e[37m[✓]\e[0m \e[32m$1\e[0m"
+}
+
+function displayWarning {
+    echo -e "\e[43m\e[37m[⚠]\e[0m \e[33m$1\e[0m"
+}
+
+function displayError {
+    echo -e "\e[41m\e[37m[x]\e[0m \e[31m$1\e[0m"
+}
