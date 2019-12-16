82eb7577511adc236be6fe59f8cb96310313726f
Daniel Campoverde <alx@sillybytes.net>
2019-12-16 10:33:39 -0500

    Scripts: pretty info for non-battery hosts

--- scripts/.scripts/echo_info.sh
+++ scripts/.scripts/echo_info.sh
@@ -4,18 +4,31 @@ source "$(dirname "$0")/volume.sh"
 
 date=$(date "+%a  %b/%d  %l:%M ")
 
-battery_state=$(~/.scripts/echo_battery.sh)
 sound_state=$(vol_echo_formatted)
+
+battery_state_raw=$( acpi -b | awk '{ print $3, $4 }' | tr -d ',' )
+battery_state=$( echo $battery_state_raw | sed \
+'s/Full/F/;s/Charging/⚡/;s/Discharging/⚠/' )
 
 if [[ "$1" == "--no-date" ]];
 then
-    echo "$sound_state | $battery_state"
+    if [[ "$battery_state" == "" ]];
+    then
+        echo "$sound_state"
+    else
+        echo "$sound_state | $battery_state"
+    fi
 
 elif [[ "$1" == "--date-only" ]];
 then
     echo "$date"
 
 else
-    echo "$sound_state | $battery_state | $date"
+    if [[ "$battery_state" == "" ]];
+    then
+        echo "$sound_state | $date"
+    else
+        echo "$sound_state | $battery_state | $date"
+    fi
 fi
 tmux refresh-client -S
