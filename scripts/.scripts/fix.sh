529f35e74b9e25d0e1da66c32644a6476782caa4
Daniel Campoverde <alx@sillybytes.net>
2019-12-16 11:21:25 -0500

    Scripts: git commit history fix

---
+++ scripts/.scripts/fix.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+WRONG_EMAIL="some@email.com"
+NEW_NAME="Daniel Campoverde"
+NEW_EMAIL="alx@sillybytes.net"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
