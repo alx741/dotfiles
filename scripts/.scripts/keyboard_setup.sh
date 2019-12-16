16ffcf2b12b1da36e1f27db8ecffae32cc8fd557
Daniel Campoverde <alx@sillybytes.net>
2019-12-16 10:45:07 -0500

    Scripts: handle hosts with no builtin keyboard

--- scripts/.scripts/keyboard_setup.sh
+++ scripts/.scripts/keyboard_setup.sh
@@ -3,9 +3,6 @@
 builtin_id=$(xinput -list | grep -i 'AT Translated' | grep -oh 'id=[0-9]*' | cut -d'=' -f 2)
 venabili_id=$(xinput -list | grep -i 'Venabili' | tail -n 1 | grep -oh 'id=[0-9]*' | cut -d'=' -f 2)
 
-echo "$builtin_id"
-echo "$venabili_id"
-
 # Repeat rate
 xset r rate 200 50
 
@@ -15,10 +12,17 @@ xbindkeys&
 # Set us-international-altgr keyboard
 setxkbmap us -variant altgr-intl
 
-# Configure keys
-setxkbmap -device "$builtin_id" -option ctrl:swapcaps&
-# xmodmap ~/.xmodmap&
+### Configure keys
 killall xcape
-xcape -t 250 -e 'Control_L=Escape'
+
+# Host has builtin keyboard
+if [ ! -z "$builtin_id" ];
+then
+    setxkbmap -device "$builtin_id" -option ctrl:swapcaps&
+    setxkbmap -option ctrl:swapcaps&
+    xcape -t 250 -e 'Control_L=Escape'
+fi
+
+# xmodmap ~/.xmodmap&
 xcape -e 'Super_L=Control_L|G|bracketleft'
 xcape -e 'Super_R=Control_R|G|bracketright'
