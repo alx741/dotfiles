d5b674eb7da47f3e765c0d30b2cb6b34afd92568
Daniel Campoverde <alx@sillybytes.net>
2020-04-06 13:48:05 -0500

    Scripts: use kernel battery information

--- scripts/.scripts/echo_info.sh
+++ scripts/.scripts/echo_info.sh
@@ -1,34 +1,11 @@
 #! /bin/sh
 
-source "$(dirname "$0")/volume.sh"
-
 date=$(date "+%a  %b/%d  %l:%M ")
+sound_state=$($(dirname "$0")/volume.sh info)
 
-sound_state=$(vol_echo_formatted)
+battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+battery_state=$(cat /sys/class/power_supply/BAT0/status \
+    | sed 's/Full/F/;s/Charging/⚡/;s/Discharging/⚠/;s/Unknown/U/;s/^ //')
 
-battery_state_raw=$( acpi -b | awk '{ print $3, $4 }' | tr -d ',' )
-battery_state=$( echo $battery_state_raw | sed \
-'s/Full/F/;s/Charging/⚡/;s/Discharging/⚠/' )
-
-if [[ "$1" == "--no-date" ]];
-then
-    if [[ "$battery_state" == "" ]];
-    then
-        echo "$sound_state"
-    else
-        echo "$sound_state | $battery_state"
-    fi
-
-elif [[ "$1" == "--date-only" ]];
-then
-    echo "$date"
-
-else
-    if [[ "$battery_state" == "" ]];
-    then
-        echo "$sound_state | $date"
-    else
-        echo "$sound_state | $battery_state | $date"
-    fi
-fi
+echo "$sound_state | $battery_state $battery_capacity | $date"
 tmux refresh-client -S
