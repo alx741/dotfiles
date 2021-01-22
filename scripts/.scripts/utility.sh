331b801db32f6506a47f928317fea900307c829f
Daniel Campoverde <alx@sillybytes.net>
2021-01-22 18:13:12 -0500

    Scripts: fix screen detection

--- scripts/.scripts/utility.sh
+++ scripts/.scripts/utility.sh
@@ -1,6 +1,6 @@
 function is_vga_plugedin
 {
-    VGASTATE=$(xrandr | grep -i 'vga' | awk '{print $2 }')
+    VGASTATE=$(xrandr | grep -i 'DP1' | awk '{print $2 }' | head -n 1)
 
     if [[ $VGASTATE == "connected" ]]; then
         return 0
@@ -12,7 +12,7 @@ function is_vga_plugedin
 
 function is_hdmi_plugedin
 {
-    VGASTATE=$(xrandr | grep -i 'hdmi' | awk '{print $2 }')
+    VGASTATE=$(xrandr | grep -i 'DP2-1' | awk '{print $2 }' | head -n 1)
 
     if [[ $VGASTATE == "connected" ]]; then
         return 0
@@ -24,10 +24,12 @@ function is_hdmi_plugedin
 function am_i_at_home
 {
     # Both VGA and HDMI ports are hooked in
-    if is_vga_plugedin && is_hdmi_plugedin
-    then
-        return 0
-    else
-        return 1
-    fi
+    # TODO: reimplement this
+    return 0
+    #if is_vga_plugedin && is_hdmi_plugedin
+    #then
+    #    return 0
+    #else
+    #    return 1
+    #fi
 }
