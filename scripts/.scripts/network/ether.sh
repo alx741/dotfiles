0ee99653a5cb9724f243c70dce78b119112b4986
Daniel Campoverde <alx@sillybytes.net>
2021-01-22 18:15:52 -0500

    Scripts: use ip interface

--- scripts/.scripts/network/ether.sh
+++ scripts/.scripts/network/ether.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-amy_addr="192.168.1.50"
+if="$1"
 
-sudo ifconfig enp3s0 $amy_addr
-sudo route add default gw 192.168.1.1
-echo 'nameserver 192.168.1.5' | sudo tee /etc/resolv.conf
+amy_addr="192.168.1.50/24"
+gw="192.168.1.1"
+
+sudo ip address add "$amy_addr" dev "$if"
+sudo ip link set "$if" up
+sudo ip route add default via "$gw" dev "$if"
+echo 'nameserver 1.1.1.1' | sudo tee /etc/resolv.conf
