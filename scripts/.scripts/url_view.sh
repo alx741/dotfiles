e78467cea60c9155a647af525180935f8d6574e5
Daniel Campoverde <alx@sillybytes.net>
2021-01-22 18:13:48 -0500

    Scripts: PDF by URL viewer

--- scripts/.scripts/url_view.sh
+++ scripts/.scripts/url_view.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-# If the URL is an image open it with SXIV
-# TODO: Use zathura when is a PDF
-
 function is_image
 {
-    if [[ `echo "$1" | grep -i -e ".jpg" -e ".png" -e ".gif" -e ".jpe" -e ".jpeg" -e ".bmp"` ]];
+    if [[ $(echo "$1" | grep -i -e ".jpg" -e ".png" -e ".gif" -e ".jpe" -e ".jpeg" -e ".bmp") ]];
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+function is_pdf
+{
+    if [[ $(echo "$1" | grep -i -e ".pdf") ]];
     then
         return 0
     else
@@ -16,6 +23,9 @@ function is_image
 if is_image "$1"
 then
     wget -t 1 "$1" -O /tmp/wgeted-image > /dev/null && sxiv -a -q /tmp/wgeted-image
+elif is_pdf "$1"
+then
+    wget -t 1 "$1" -O /tmp/wgeted-pdf > /dev/null && zathura -a -q /tmp/wgeted-pdf
 else
     firefox "$1"
 fi
