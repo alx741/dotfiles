03ff33e8bc1ec761e818971a74e6d6bb7be7054f
Daniel Campoverde <alx@sillybytes.net>
2021-01-22 18:10:36 -0500

    Scripts: report generator with bibliography

--- scripts/.scripts/generate_report.sh
+++ scripts/.scripts/generate_report.sh
@@ -2,26 +2,53 @@
 
 source "$(dirname "$0")/fancy.sh"
 
-if [[ -f report.md ]]; then
-    displayWarning "A report already exists!"
-    exit 1
-fi
+[ -f report.md ] && displayWarning "A report already exists!" && exit 1
 
-made markdown
+made markdown || exit 1
 
-if [[ $? != 0 ]]; then
-    displayError "Can't create Makefile! Maybe one already exists?"
-    exit 1
-fi
-
+# Report template
 cat <<EOF > report.md
 ---
 title:
 author: Daniel Campoverde C.
+date: $(date +'%B %Y')
+lang: es
+bibliography: bibliography.bib
 abstract: |
     Some Abstract here
+numbersections: true
 ...
 
+\tableofcontents
+
+# Bibliografía
+EOF
+
+
+# Bibliography template
+cat <<EOF > bibliography.bib
+@online{some,
+    title={},
+    journal={},
+    author={Last, Given},
+    year={2015},
+    month={1},
+    url={},
+    urldate={2020-01}
+}
+
+@article{some,
+    title={},
+    journal={},
+    author={Last, Given},
+    year={2015}
+}
+EOF
+
+
+# gitignore template
+cat <<EOF > .gitignore
+report.pdf
 EOF
 
 mkdir img
