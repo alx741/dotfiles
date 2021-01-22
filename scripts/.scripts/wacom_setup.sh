1c56c4713aa69443465ca511922b4bdadebed7a3
Daniel Campoverde <alx@sillybytes.net>
2021-01-22 18:16:37 -0500

    Scripts: add Wacom tablet setup

---
+++ scripts/.scripts/wacom_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+WACOM_PEN="Wacom Intuos S Pen stylus"
+WACOM_PAD="Wacom Intuos S Pad pad"
+MAIN_SCREEN="DP2-1"
+
+xsetwacom set "$WACOM_PEN" MapToOutput "$MAIN_SCREEN"
+xsetwacom set "$WACOM_PEN" Button 2 2
+xsetwacom set "$WACOM_PEN" Button 3 3
+
+xsetwacom set "$WACOM_PAD" Button 1 "key +ctrl z -ctrl"
+xsetwacom set "$WACOM_PAD" Button 2 "key +ctrl +shift z -ctrl -shift"
+xsetwacom set "$WACOM_PAD" Button 3 "key +ctrl 0 -ctrl"
+xsetwacom set "$WACOM_PAD" Button 8 "key m"
+
+xsetwacom set "$WACOM_PEN" PressureCurve 0 0 70 100
